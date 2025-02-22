5553b7ed600ed2af099c200faa7aa21cc9a73fbe
HeadlessWing <wingcue@gmail.com>
2025-02-22 13:13:30 -0800

    added words counting function

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def num_words(text):
+    text_list = text.split()
+    length = len(text_list)
+    return length
+
+    
+
 def main(): 
     text = get_book_text("./books/frankenstein.txt")
-    print(text)
+    length = num_words(text)
+    print(f"{length} words found in the document")
+    #print(text)
 
 main()
