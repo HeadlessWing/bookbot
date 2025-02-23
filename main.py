7219ff46c3ce764a86fa68752b15850ce40bd5ca
HeadlessWing <wingcue@gmail.com>
2025-02-22 18:49:24 -0800

    Added make a list function and sorting a list of dictionaries function

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents  
 
-from stats import num_words
-
-from stats import char_count
+from stats import num_words, char_count, make_list, sort_dict
 
 def main(): 
     text = get_book_text("./books/frankenstein.txt")
     length = num_words(text)
     count = char_count(text)
     print(f"{length} words found in the document")
-    #print(text)
-    print(count)
+    list = make_list(count)
+    #print(list)
+    #print(count)
+    sort_dict(list)
+    print(list)
 main()
--- stats.py
+++ stats.py
@@ -14,4 +14,17 @@ def char_count(text):
             else :
                 count[character] = 1
     return count
-        
+def make_list(dict):
+    list = []
+    for pair in dict:
+        list.append({pair:dict[pair]})
+    return list
+
+def sort_on(dict):
+        for char in dict:
+            return dict[char]
+
+def sort_dict(dict):
+    dict.sort(reverse=True, key=sort_on) 
+    return dict
+    
